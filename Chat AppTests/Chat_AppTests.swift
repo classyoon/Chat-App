5ddf586a80db8e9f93c4bfeff90cdc82eae6385c
classyoon <96508736+classyoon@users.noreply.github.com>
2026-02-13 15:30:25 -0500

    Initial Commit

---
+++ Chat AppTests/Chat_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Chat_AppTests.swift
+//  Chat AppTests
+//
+//  Created by Conner Yoon on 2/13/26.
+//
+
+import Testing
+@testable import Chat_App
+
+struct Chat_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
